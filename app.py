57447c86780055acc0da20fc92c725b8e6a7fccf
oleghasjanov <oleg.phenomenon@gmail.com>
2024-11-24 21:02:34 +0200

    remove comments

--- app.py
+++ app.py
@@ -5,26 +5,13 @@ from rvc.modules.vc.modules import VC
 import os
 import tempfile
 
-# # Настройка переменных окружения
-# os.environ["index_root"] = "/content/models"
-# os.environ["hubert_path"] = "/content/models/hubert_base.pt"
-# os.environ["weight_root"] = "/content/models"
-# os.environ["rmvpe_root"] = "/content/models"
-# os.environ["weight_uvr5_root"] = "/content/models"
-# os.environ["save_uvr_path"] = "/content/models"
-# os.environ["TEMP"] = "/content/temp"
-# os.environ["pretrained"] = "/content/models/hubert_base.pth"
-
-# Инициализация Flask
 app = Flask(__name__)
 
-# Инициализация модели
 vc = VC()
-vc.get_vc("/content/models/kow.pth")
+vc.get_vc("/root/rvc_converter_flask/rvc_models/kowalski.pth")
 
 @app.route("/convert", methods=["POST"])
 def convert_audio():
-    # Проверяем, есть ли файл в запросе
     if "file" not in request.files:
         return jsonify({"error": "No file provided"}), 400
 
@@ -32,33 +19,27 @@ def convert_audio():
     if file.filename == "":
         return jsonify({"error": "No file selected"}), 400
 
-    # Сохраняем загруженный файл во временную директорию
     with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_in:
         input_path = temp_in.name
         file.save(input_path)
 
     try:
-        # Выполняем конвертацию
         tgt_sr, audio_opt, _, _ = vc.vc_single(1, Path(input_path))
 
-        # Сохраняем результат во временный файл
         with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_out:
             output_path = temp_out.name
             wavfile.write(output_path, tgt_sr, audio_opt)
 
-        # Отправляем файл обратно клиенту
         return send_file(output_path, as_attachment=True, download_name="converted.wav", mimetype="audio/wav")
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
     finally:
-        # Удаляем временные файлы
         if os.path.exists(input_path):
             os.remove(input_path)
         if os.path.exists(output_path):
             os.remove(output_path)
 
-# Запуск приложения
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
